8cdd9245af56afa2bf6b0e363c6d09de25284351
José Victor Pereira Lima <josevictorp81@gmail.com>
2022-01-08 22:06:05 -0300

    layout finalizado

---
+++ lib/dice.dart
@@ -0,0 +1,58 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class Dice extends StatefulWidget {
+  const Dice({Key? key}) : super(key: key);
+
+  @override
+  _DiceState createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int leftDice = Random().nextInt(6) + 1;
+  int rightDice = Random().nextInt(6) + 1;
+
+  void chooseNumber() {
+    setState(() {
+      leftDice = Random().nextInt(6) + 1;
+      rightDice = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dicee'),
+        centerTitle: true,
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.red,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            GestureDetector(
+              onTap: chooseNumber,
+              child: SizedBox(
+                width: 140,
+                child: Image.asset('images/dice$leftDice.png'),
+              ),
+            ),
+            // Padding(
+            //   padding: EdgeInsets.only(right: 15),
+            // ),
+            GestureDetector(
+              onTap: chooseNumber,
+              child: SizedBox(
+                width: 140,
+                child: Image.asset('images/dice$rightDice.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
